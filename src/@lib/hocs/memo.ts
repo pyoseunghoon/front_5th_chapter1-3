7d73c51f4d186b8f36251423ecba121cfdfb1a8d
pyoseunghoon <55734481+pyoseunghoon@users.noreply.github.com>
2025-04-06 14:48:26 +0900

    memo 구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -2,30 +2,43 @@
 import { shallowEquals } from "../equalities";
 //  React 컴포넌트의 타입을 정의할 때 사용하는 제네릭 타입
 // 컴포넌트가 받을 수 있는 props 타입을 자동으로 추론해주는 유용한 타입
-import { ComponentType } from "react";
+import React, {ComponentType, ReactElement} from "react";
 
 // 현재 jsx를 자동변환해주는 것이 없어 사용 x
 // function renderComponent<P>(Component: ComponentType<P>, props: P) {
 //   return <Component {...props} />;
 // }
 
-// 컴포넌트 자체를 메모이제이션
-export function memo<P extends object>(
+/**
+ * 컴포넌트 자체를 메모이제이션
+ * prevProps, prevElement를 클로저를 사용하여 외부에서 접근 불가하도록
+ */
+export function memo<P extends object | null>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  let previousProps: P;
+  // 1. 이전 props를 저장할 ref 생성
+  let prevProps: P | null = null;
+  let prevElement: ReactElement;
 
-  // console.log('memo');
-  // console.log('memo comp', Component);
-  return function MemoizedComponent(props: P) {
-    // console.log('memo props', props);
-    if (_equals(previousProps, props)) {
+  // 2. 메모이제이션된 컴포넌트 생성 함수 리턴
+  return function MemoizedComponent(props: P | null) {
+    // console.log('prevElement', prevElement);
+
+    // console.log('prevProps', prevProps);
+    // console.log('props', props);
+
+    // 2-1. equals 함수를 사용하여 props 비교
+    if (_equals(prevProps, props)) {
       return null;
     }
 
-    previousProps = props;
-    // props가 변경된 경우, 컴포넌트를 렌더링
-    return React.createElement(Component, props);
+    // 2-2. props가 변경된 경우에만 새로운 렌더링 수행
+    const element = React.createElement(Component, props);
+    // console.log('element', element);
+
+    prevProps = props;
+    prevElement = element;
+    return element;
   };
 }
